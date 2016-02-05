4041f67e02ef38263fcd7daee9b3e4a8533baa01
Libby Jaffa <ljaffa@student.touro.edu>
2016-02-04 20:40:45 -0500

    initial maven files

---
+++ src/test/java/jaffa/mco364/AppTest.java
@@ -0,0 +1,16 @@
+package jaffa.mco364;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
---
+++ src/test/java/jaffa/mco364/todo/TodoServiceTest.java
@@ -0,0 +1,35 @@
+package jaffa.mco364.todo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class TodoServiceTest {
+
+	// should verify that you download 200 todo items
+	@Test
+	public void testListTodos() throws IOException {
+		Retrofit retrofit = new Retrofit.Builder()
+		.baseUrl("http://jsonplaceholder.typicode.com")
+		.addConverterFactory(GsonConverterFactory.create()).build();
+
+		TodoService service = retrofit.create(TodoService.class);
+
+		Call<List<Todo>> call = service.listTodos();
+
+		Response<List<Todo>> response = call.execute();
+
+		List<Todo> list = response.body();
+
+		assertEquals(200, list.size());
+	}
+
+}
